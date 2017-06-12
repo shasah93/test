2722699bcd7e29bf1a6c169d64129a0cc0465817
shasah93 <347216329@qq.com>
2017-06-12 16:03:56 +0800

    自动生成多篇博客

---
+++ build.js
@@ -0,0 +1,15 @@
+const fs=require('fs')
+const path=require('path')
+fs.readdir('./markdown',function(error,files){
+	for (var i = 0; i < files.length; i++) {
+		
+	
+        var p=path.join('./markdown',files[i])
+	var markdown=fs.readFileSync(p).toString()
+    var template=fs.readFileSync('./template.html').toString()
+    var result=template.replace('%content%',markdown)
+    //console.log(result)
+    fs.writeFileSync(files[i]+'.html',result)}
+})
+
+
